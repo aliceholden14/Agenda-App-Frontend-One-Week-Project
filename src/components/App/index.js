e2463d9898abbb775992db87e94a9e4b4df92df6
= <=>
2020-11-11 11:09:04 +0000

    alice

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Form from "../Form/index";
 import NoteList from "../NoteList/index";
 import Agenda from "../Agenda/index";
@@ -7,6 +7,30 @@ import Agenda from "../Agenda/index";
 function App() {
   const [notes, setNotes] = useState([]);
   const [agenda, setAgenda] = useState([]);
+
+  async function getApiData() {
+    const res = await fetch("http://localhost:5000/notes");
+    const dataArray = await res.json();
+    //console.log(dataArray.data.rows);
+    return dataArray.data.rows;
+  }
+
+  async function loadApiData() {
+    const dataArray = await getApiData();
+    for (let i = 0; i < dataArray.length; i++) {
+      dataArray[i].dateTime = dataArray[i]["date"];
+      delete dataArray[i].date;
+      dataArray[i].onAgenda = dataArray[i]["on_agenda"];
+      delete dataArray[i].on_agenda;
+    }
+    return dataArray;
+  }
+
+  useEffect(() => {
+    const data = loadApiData();
+    console.log(data);
+    setNotes([]);
+  }, []);
 
   function addLi(formEntry) {
     setNotes([...notes, formEntry]);
@@ -29,6 +53,7 @@ function App() {
       <Form addLi={addLi} addToAgenda={addToAgenda} />
       <NoteList notes={notes} deleteLi={deleteLi} addToAgenda={addToAgenda} />
       <Agenda agenda={agenda} deleteFromAgenda={deleteFromAgenda} />
+      <button onClick={() => loadApiData(getApiData())}>TEST BUTTON</button>
     </div>
   );
 }
