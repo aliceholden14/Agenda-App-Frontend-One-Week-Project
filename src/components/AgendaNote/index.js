3657a204b01bdb22bcd607733255274c29ce68e5
= <=>
2020-11-12 13:43:17 +0000

    beforeLunch

--- src/components/AgendaNote/index.js
+++ src/components/AgendaNote/index.js
@@ -3,19 +3,33 @@ import "../style.css";
 
 function AgendaNote({ idx, text, deleteFromAgenda }) {
   return (
-    <li key={idx}>
-      <h4>{text.category}</h4>
-      <h4>{text.priority}</h4>
-      <h3>{text.title}</h3>
-      <p>{text.description}</p>
-      <p>{text.dateTime}</p>
-      <button
-        onClick={() => {
-          return deleteFromAgenda(idx);
-        }}
-      >
-        🚫
-      </button>
+    <li key={idx} id="agendaItem">
+      <div id="priegory">
+        <h4>{text.category}</h4>
+        <h4>{text.priority}</h4>
+      </div>
+      <div id="noteInfo">
+        <div id="agnTitle">
+          <h3>{text.title}</h3>
+        </div>
+        <div id="agnDesc">
+          <p>{text.description}</p>
+        </div>
+        <div id="agnFooter">
+          <div id="agnButton">
+            <button
+              onClick={() => {
+                return deleteFromAgenda(idx);
+              }}
+            >
+              🚫
+            </button>
+          </div>
+          <div id="agnDT">
+            <p>{text.dateTime}</p>
+          </div>
+        </div>
+      </div>
     </li>
   );
 }
